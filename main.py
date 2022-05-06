fbdb314240aa3aabf823841421f7a0e426417399
ProFunDS <68074080+ProFunDS@users.noreply.github.com>
2022-05-06 17:33:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,25 @@
 from time import sleep
 
 from vk_api import VkApi
+from vk_api.vk_api import VkApiMethod
 
-from config import login, password, token # access_token с правами доступа: wall.
+from config import login, password, token  # access_token с правами доступа: wall.
 
 
-def two_factor():
+def two_factor() -> tuple[str, bool]:
     code = input('Код аутентификации: ')
-    return code, True # True - сохранить сессию в локальном файле.
+    return code, True  # True - сохранить сессию в локальном файле.
 
-def enter():
+def enter() -> VkApiMethod:
     vk_session = VkApi(login, password, token=token, auth_handler=two_factor)
     vk_session.auth()
-    vk = vk_session.get_api()
-    return vk
+    return vk_session.get_api()
 
-def delete_reposts(vk, walls):
+def delete_reposts(vk: VkApiMethod, walls: dict) -> None:
     length = len(walls)
     counter = 1
     for wall in walls:
-        if 'copy_history' not in wall: # это не репост
+        if 'copy_history' not in wall:  # это не репост
             length -= 1
             continue
             
@@ -36,8 +36,6 @@ def delete_reposts(vk, walls):
 
 if __name__ == '__main__':
     vk = enter()
-    for i in range(1, 11): # первая тысяча постов
-        print(f'[!]    Партия постов №{i}    [!]')
-        walls = vk.wall.get(count=100, filter='owner')['items']
-        delete_reposts(vk, walls)
-        sleep(3)
+    COUNT = 100  # постов (макс. для одного запроса)
+    walls = vk.wall.get(count=COUNT, filter='owner')['items']
+    delete_reposts(vk, walls)
